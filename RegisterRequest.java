66d2a593bc160f2a4a1acf790ef61aedc69ec835
Shrabanee Routray <shrabaneeroutray@gmail.com>
2026-06-09 22:14:18 +0530

    Add files via upload

---
+++ RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.example.lifeloopbackend.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class RegisterRequest {
+
+    private String name;
+    private String email;
+    private String password;
+}
